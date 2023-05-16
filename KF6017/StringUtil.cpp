98730a57d2c57f0552de2dddb3dccedf7ef0f729
Marek Kočvara <kocvara.mark@gmail.com>
2023-05-16 09:13:36 +0100

    StringUtil fix

--- KF6017/StringUtil.cpp
+++ KF6017/StringUtil.cpp
@@ -1,42 +1,42 @@
 #include "StringUtil.h"
 
 
-// Trim functions from https://stackoverflow.com/a/217605
+// Trim functions adapted from https://stackoverflow.com/a/217605
  
 // trim from start (in place)
-inline void StringUtil::ltrim(std::string& s) {
+void StringUtil::ltrim(std::string& s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
         return !std::isspace(ch);
         }));
 }
 
 // trim from end (in place)
-inline void StringUtil::rtrim(std::string& s) {
+void StringUtil::rtrim(std::string& s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
         return !std::isspace(ch);
         }).base(), s.end());
 }
 
 // trim from both ends (in place)
-inline void StringUtil::trim(std::string& s) {
+void StringUtil::trim(std::string& s) {
     rtrim(s);
     ltrim(s);
 }
 
 // trim from start (copying)
-inline std::string StringUtil::ltrim_copy(std::string s) {
+std::string StringUtil::ltrim_copy(std::string s) {
     ltrim(s);
     return s;
 }
 
 // trim from end (copying)
-inline std::string StringUtil::rtrim_copy(std::string s) {
+std::string StringUtil::rtrim_copy(std::string s) {
     rtrim(s);
     return s;
 }
 
 // trim from both ends (copying)
-inline std::string StringUtil::trim_copy(std::string s) {
+std::string StringUtil::trim_copy(std::string s) {
     trim(s);
     return s;
 }
--- KF6017/StringUtil.h
+++ KF6017/StringUtil.h
@@ -5,21 +5,21 @@ class StringUtil
 {
 public:
 	// trim from start (in place)
-    static inline void ltrim(std::string& s);
+    static void ltrim(std::string& s);
 
 // trim from end (in place)
-    static inline void rtrim(std::string& s);
+    static void rtrim(std::string& s);
 
 // trim from both ends (in place)
-    static inline void trim(std::string& s);
+    static void trim(std::string& s);
 
 // trim from start (copying)
-    static inline std::string ltrim_copy(std::string s);
+    static std::string ltrim_copy(std::string s);
 
 // trim from end (copying)
-    static inline std::string rtrim_copy(std::string s);
+    static std::string rtrim_copy(std::string s);
 
 // trim from both ends (copying)
-    static inline std::string trim_copy(std::string s);
+    static std::string trim_copy(std::string s);
 };
 
